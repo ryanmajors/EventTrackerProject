c8974e618490ff9e5c918f07759e3d04ac244bf2
Ryan Majors <growbox.ryan@yahoo.com>
2022-01-07 15:49:56 -0700

    Configure JPA project dependency

---
+++ DGTournamentTracker/src/main/java/com/skilldistillery/dgtournament/DgTournamentTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dgtournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgTournamentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgTournamentTrackerApplication.class, args);
+	}
+
+}
