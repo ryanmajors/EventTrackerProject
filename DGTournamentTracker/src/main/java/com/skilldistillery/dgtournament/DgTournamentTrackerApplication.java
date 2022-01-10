a71c225d7e12ceb42cc97a5101c01ef8629127ef
Ryan Majors <growbox.ryan@yahoo.com>
2022-01-10 11:55:18 -0700

    Deployed to aws and fixed urls in the README.md

--- DGTournamentTracker/src/main/java/com/skilldistillery/dgtournament/DgTournamentTrackerApplication.java
+++ DGTournamentTracker/src/main/java/com/skilldistillery/dgtournament/DgTournamentTrackerApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.dgtournament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DgTournamentTrackerApplication {
+public class DgTournamentTrackerApplication extends SpringBootServletInitializer {
+
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(DgTournamentTrackerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DgTournamentTrackerApplication.class, args);
