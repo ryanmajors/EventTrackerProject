a4451b2f6d265798ce783c5f8b49ed455733c039
Ryan Majors <growbox.ryan@yahoo.com>
2022-01-15 17:55:14 -0700

    Added newTournament form and created functionality for creating new tournament to persist to the database, tested successfully

--- DGTournamentTracker/src/main/resources/static/js/script.js
+++ DGTournamentTracker/src/main/resources/static/js/script.js
@@ -22,16 +22,23 @@ function init() {
     getAllTournaments();
   });
 
+  let newTournamentButton = document.getElementById("addTournamentButton");
+  newTournamentButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    displayNewTournamentForm();
+  });
+
+  let addNewTournamentButton = document.getElementById('')
+
   let year2021 = document.getElementById("year2021");
   year2021.addEventListener("click", function (e) {
     e.preventDefault();
     let year = year2021.textContent;
     getAllTournamentData(year);
-    let seasonTotalYear = document.getElementById('seasonTotals');
+    let seasonTotalYear = document.getElementById("seasonTotals");
     seasonTotalYear.textContent = year + " Season Totals";
-    let tournamentResultsYear = document.getElementById('tournamentResults');
+    let tournamentResultsYear = document.getElementById("tournamentResults");
     tournamentResultsYear.textContent = year + " Tournament Results";
-
   });
 
   let year2020 = document.getElementById("year2020");
@@ -39,9 +46,9 @@ function init() {
     e.preventDefault();
     let year = year2020.textContent;
     getAllTournamentData(year);
-    let seasonTotalYear = document.getElementById('seasonTotals');
+    let seasonTotalYear = document.getElementById("seasonTotals");
     seasonTotalYear.textContent = year + " Season Totals";
-    let tournamentResultsYear = document.getElementById('tournamentResults');
+    let tournamentResultsYear = document.getElementById("tournamentResults");
     tournamentResultsYear.textContent = year + " Tournament Results";
   });
 
@@ -50,9 +57,9 @@ function init() {
     e.preventDefault();
     let year = year2019.textContent;
     getAllTournamentData(year);
-    let seasonTotalYear = document.getElementById('seasonTotals');
+    let seasonTotalYear = document.getElementById("seasonTotals");
     seasonTotalYear.textContent = year + " Season Totals";
-    let tournamentResultsYear = document.getElementById('tournamentResults');
+    let tournamentResultsYear = document.getElementById("tournamentResults");
     tournamentResultsYear.textContent = year + " Tournament Results";
   });
 
@@ -61,9 +68,9 @@ function init() {
     e.preventDefault();
     let year = year2018.textContent;
     getAllTournamentData(year);
-    let seasonTotalYear = document.getElementById('seasonTotals');
+    let seasonTotalYear = document.getElementById("seasonTotals");
     seasonTotalYear.textContent = year + " Season Totals";
-    let tournamentResultsYear = document.getElementById('tournamentResults');
+    let tournamentResultsYear = document.getElementById("tournamentResults");
     tournamentResultsYear.textContent = year + " Tournament Results";
   });
 }
@@ -126,6 +133,125 @@ function getAllTournamentData(year) {
   xhr.send();
 }
 
+function createTournament(newTournament) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "api/tournaments");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200 || xhr.status === 201) {
+        let tournament = JSON.parse(xhr.responseText);
+        console.log(tournament)
+        getAllTournamentData(2021);
+      } else {
+        console.error("tournament create failed with status: " + xhr.status);
+      }
+    }
+  };
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.send(JSON.stringify(newTournament))
+}
+
+function displayNewTournamentForm() {
+  let addNewTournamentTitle = document.getElementById("tournamentResults");
+  addNewTournamentTitle.textContent = "Add New Tournament Form";
+
+  let tournamentFormData = document.getElementById("tournamentData");
+  tournamentFormData.textContent = "";
+
+  let tournamentForm = document.createElement("form");
+  tournamentForm.id = "addTournamentForm";
+  tournamentForm.name = tournamentForm.id;
+  tournamentFormData.appendChild(tournamentForm);
+
+  let nameLabel = document.createElement("label");
+  nameLabel.for = "nameField";
+  nameLabel.textContent = "Tournament Name";
+  tournamentForm.appendChild(nameLabel);
+
+  let nameInput = document.createElement("input");
+  nameInput.class = "addTournamentFormInputFields";
+  nameInput.type = "text";
+  nameInput.name = "nameField";
+  tournamentForm.appendChild(nameInput);
+
+  let nameBr = document.createElement("br");
+  tournamentForm.appendChild(nameBr);
+
+  let tierLabel = document.createElement("label");
+  tierLabel.for = "tierField";
+  tierLabel.textContent = "Tier";
+  tournamentForm.appendChild(tierLabel);
+
+  let tierInput = document.createElement("input");
+  tierInput.class = "addTournamentFormInputFields";
+  tierInput.type = "text";
+  tierInput.name = "tierField";
+  tournamentForm.appendChild(tierInput);
+
+  let tierBr = document.createElement("br");
+  tournamentForm.appendChild(tierBr);
+
+  let locationLabel = document.createElement("label");
+  locationLabel.for = "locationField";
+  locationLabel.textContent = "Location";
+  tournamentForm.appendChild(locationLabel);
+
+  let locationInput = document.createElement("input");
+  locationInput.class = "addTournamentFormInputFields";
+  locationInput.type = "text";
+  locationInput.name = "locationField";
+  tournamentForm.appendChild(locationInput);
+
+  let locationBr = document.createElement("br");
+  tournamentForm.appendChild(locationBr);
+
+  let monthLabel = document.createElement("label");
+  monthLabel.for = "monthField";
+  monthLabel.textContent = "Month";
+  tournamentForm.appendChild(monthLabel);
+
+  let monthInput = document.createElement("input");
+  monthInput.class = "addTournamentFormInputFields";
+  monthInput.type = "text";
+  monthInput.name = "monthField";
+  tournamentForm.appendChild(monthInput);
+
+  let monthBr = document.createElement("br");
+  tournamentForm.appendChild(monthBr);
+
+  let yearLabel = document.createElement("label");
+  yearLabel.for = "yearField";
+  yearLabel.textContent = "Year";
+  tournamentForm.appendChild(yearLabel);
+
+  let yearInput = document.createElement("input");
+  yearInput.class = "addTournamentFormInputFields";
+  yearInput.type = "text";
+  yearInput.name = "yearField";
+  tournamentForm.appendChild(yearInput);
+
+  let yearBr = document.createElement("br");
+  tournamentForm.appendChild(yearBr);
+
+  let addTournamentButton = document.createElement("input");
+  addTournamentButton.type = "submit";
+  addTournamentButton.name = "createTournament";
+  addTournamentButton.value = "Submit";
+  tournamentForm.appendChild(addTournamentButton);
+
+  addTournamentButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let f = tournamentForm;
+    let newTournament = {};
+    newTournament.name = f.nameField.value;
+    newTournament.tier = f.tierField.value;
+    newTournament.location = f.locationField.value;
+    newTournament.month = f.monthField.value;
+    newTournament.year = f.yearField.value;
+    createTournament(newTournament);
+  });
+}
+
 // display season totals for basic stats by year
 function displayBasicStatsTable(tournaments) {
   let basicStatData = document.getElementById("statData");
@@ -167,14 +293,14 @@ function displayBasicStatsTable(tournaments) {
   let played = document.createElement("td");
   played.textContent = tournaments.length;
   row.appendChild(played);
-  
+
   let pointsPerSeasonTotal = 0;
   let seasonWins = 0;
 
   for (const tournament of tournaments) {
     pointsPerSeasonTotal += tournament.points;
-    if(tournament.placement === 1) {
-      winCounter = 1
+    if (tournament.placement === 1) {
+      winCounter = 1;
       seasonWins += winCounter;
     }
   }
@@ -184,10 +310,11 @@ function displayBasicStatsTable(tournaments) {
   row.appendChild(pointsPerSeason);
 
   let avgPointsPerSeason = document.createElement("td");
-  avgPointsPerSeason.textContent = Math.round((pointsPerSeasonTotal / tournaments.length) * 100) / 100;
+  avgPointsPerSeason.textContent =
+    Math.round((pointsPerSeasonTotal / tournaments.length) * 100) / 100;
   row.appendChild(avgPointsPerSeason);
 
-  let winsPerSeason = document.createElement('td');
+  let winsPerSeason = document.createElement("td");
   winsPerSeason.textContent = seasonWins;
   row.appendChild(winsPerSeason);
 }
@@ -227,17 +354,16 @@ function displayTournamentPage(tournament) {
   players.textContent = "Players: " + tournament.players;
   tournamentData.appendChild(players);
 
-  let br = document.createElement('br');
+  let br = document.createElement("br");
   tournamentData.appendChild(br);
 
-  let updateButton = document.createElement('button');
+  let updateButton = document.createElement("button");
   updateButton.textContent = "UPDATE";
   tournamentData.appendChild(updateButton);
 
-  let deleteButton = document.createElement('button');
+  let deleteButton = document.createElement("button");
   deleteButton.textContent = "DELETE";
   tournamentData.appendChild(deleteButton);
-
 }
 
 function displayTournamentsTable(tournaments) {
@@ -300,10 +426,10 @@ function displayTournamentsTable(tournaments) {
     let date = document.createElement("td");
     date.textContent = tournament.month + "/" + tournament.year;
     row.appendChild(date);
-    row.addEventListener('click', function(e) {
+    row.addEventListener("click", function (e) {
       e.preventDefault();
       getTournament(tournament.id);
-    })
+    });
     tBody.appendChild(row);
   }
 }
